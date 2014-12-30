5d47843b75ad4e41e13c01157cb6d73d1015732b
fxi <f@fxi.io>
2014-12-30 14:19:27 +0100

    added compilation of grass, virtio networks adapters

--- Vagrantfile
+++ Vagrantfile
@@ -11,7 +11,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provider "virtualbox" do |v|
     v.memory = 2048
     v.cpus = 2
-    v.name = "accessModServer"
+    v.name = "accessmodServer"
   end
   # provision : shell script. Move to something like Chef Solo provisioner
   config.vm.provision "shell", path: "provision.sh"
@@ -25,7 +25,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
   config.vm.network "private_network", ip: "192.168.38.38"
-
+  config.vm.hostname = "accessmod"
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
   # your network.
@@ -39,8 +39,19 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  config.vm.synced_folder "/Users/fxi/Public/share", "/sharedFile"
-  config.vm.synced_folder "/Users/fxi/accessMod5Link/accessmodServer", "/sharedConfig"
- end
+  config.vm.synced_folder "/Users/fxi/Public/share/maps", "/sharedFile"
+  config.vm.synced_folder "/Users/fxi/accessMod5Link/github/accessmodServer.git", "/sharedConfig"
+
+  # http://serverfault.com/questions/495914/vagrant-slow-internet-connection-in-guest
+  config.vm.provider "virtualbox" do |v|
+    v.customize ["modifyvm", :id, "--nictype1", "virtio"]
+    v.customize ["modifyvm", :id, "--nictype2", "virtio"]
+    v.customize ["modifyvm", :id, "--nictype3", "virtio"]
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+    v.customize ["modifyvm", :id, "--chipset", "ich9"]
+  end
+
+end
 
 
--- provision.sh
+++ provision.sh
@@ -25,54 +25,128 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # apt-get.. to much failed attempt, new method: one by one.
-sudo apt-get install -y  build-essential 
-sudo apt-get install -y  curl 
-sudo apt-get install -y  gdebi-core 
-sudo apt-get install -y  pandoc 
-sudo apt-get install -y  pandoc-citeproc 
-sudo apt-get install -y  libgdal-dev 
-sudo apt-get install -y  libcurl4-gnutls-dev 
-sudo apt-get install -y  libcairo2-dev 
-sudo apt-get install -y  libxt-dev 
-sudo apt-get install -y  gdal-bin 
-sudo apt-get install -y  libgdal1-dev
-sudo apt-get install -y  libgeos-dev 
-sudo apt-get install -y  libgeos-3.4.2 
-sudo apt-get install -y  r-base 
-sudo apt-get install -y  r-base-dev 
-sudo apt-get install -y  grass
-#sudo apt-get install -y  grass70-dev 
-#sudo apt-get install -y  grass70-dev-doc
+sudo apt-get install -y build-essential 
+sudo apt-get install -y curl 
+sudo apt-get install -y gdebi-core 
+sudo apt-get install -y pandoc 
+sudo apt-get install -y pandoc-citeproc 
+sudo apt-get install -y libcurl4-gnutls-dev 
+sudo apt-get install -y libxt-dev 
+sudo apt-get install -y r-base 
+sudo apt-get install -y r-base-dev 
+sudo apt-get install -y lesstif2-dev 
+sudo apt-get install -y dpatch 
+sudo apt-get install -y libtiff4-dev 
+sudo apt-get install -y libfftw3-dev 
+sudo apt-get install -y libxmu-dev 
+sudo apt-get install -y libfreetype6-dev 
+sudo apt-get install -y autoconf2.13 
+sudo apt-get install -y autotools-dev 
+sudo apt-get install -y doxygen 
+sudo apt-get install -y libsqlite3
+sudo apt-get install -y flex 
+sudo apt-get install -y bison 
+sudo apt-get install -y libgeos-dev 
+sudo apt-get install -y libgeos-3.4.2 -dev 
+sudo apt-get install -y libcairo2-dev 
+sudo apt-get install -y gdal-bin 
+sudo apt-get install -y libgdal1-dev  
+sudo apt-get install -y libgdal1-1.5.0 
+sudo apt-get install -y libproj-dev  
+sudo apt-get install -y libproj0 
+sudo apt-get install -y proj-data 
+sudo apt-get install -y libgsl0-dev
+sudo apt-get install -y python-numpy
+sudo apt-get install -y python-dev
+sudo apt-get install -y wx-common
+sudo apt-get install -y python-wxgtk2.8
+sudo apt-get install -y git
 
+# script to install shiny server.
+
+mkdir -p $HOME/downloads
+cd $HOME/downloads
 
 # Download and install libssl 0.9.8 
-if [ `dpkg-query  -l | grep libssl0.9.8 | wc -l` -eq 0 ]
+if [ `sudo dpkg-query  -l | grep libssl0.9.8 | wc -l` -eq 0 ]
 then
   sudo wget --no-verbose http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
-    dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
-    rm -f libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
+    sudo dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
+    sudo  rm -f libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
 else
   echo "libssl0.9.8 already installed" 
 fi
 
 
 # Download and install shiny server
-if [ `dpkg-query  -l | grep shiny-server | wc -l` -eq 0 ]
+if [ `sudo dpkg-query  -l | grep shiny-server | wc -l` -eq 0 ]
 then
   echo "Download and install shiny-server"
+
   sudo wget --no-verbose https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/VERSION -O "version.txt" && \
     VERSION=$(cat version.txt)  && \
     sudo wget --no-verbose "https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/shiny-server-$VERSION-amd64.deb" -O ss-latest.deb && \
     sudo  gdebi -n ss-latest.deb && \
-    sudo rm -f version.txt ss-latest.deb && \
-    sudo R -e "install.packages(c('shiny', 'rmarkdown'), repos='http://cran.rstudio.com/')" && \
-    sudo cp -R /usr/local/lib/R/site-library/shiny/examples/* /srv/shiny-server/ && \
-    sudo echo 'options("repos"="http://cran.rstudio.com")' >> /etc/R/Rprofile.site
+    sudo  rm -f version.txt ss-latest.deb 
+  sudo echo 'options("repos"="http://cran.rstudio.com")' >> /etc/R/Rprofile.site
+
+  #sudo cp -R /usr/local/lib/R/site-library/shiny/examples/* /srv/shiny-server/
 else
   echo "Shiny-server already installed"
 fi
 
-sudo apt-get autoclean
-sudo apt-get autoremove
+# other package to downloads
+
+
+sudo R -e "install.packages(c('shiny','rmarkdown','gdalUtils','spgrass6','raster','rgdal','tools','maps','R.utils','htmltools','shinysky','devtools','plyr'))"
+sudo  R -e "library(devtools);install_github('AnalytixWare/ShinySky')"
+#sudo  R -e "library(devtools);install_github('trestletech/shinyTable')"
+
+## install accessmod shiny
+
+
+sudo mkdir -p /srv/shiny-server/data/grass
+sudo mkdir -p /srv/shiny-server/logs
+sudo touch /srv/shiny-server/logs/logs.txt
+sudo git clone https://github.com/fxi/accessModShiny.git /srv/shiny-server/accessModShiny.git
+sudo chown -R shiny:shiny /srv/shiny-server
+
+# install grass and r.walk.accessmod
+mkdir -p $HOME/downloads
+cd $HOME/downloads
+
+wget http://grass.osgeo.org/grass70/source/grass-7.0.0beta3.tar.gz
+tar xvf grass-7.0.0beta3.tar.gz
+cd grass-7.0.0beta3/
+sudo ./configure
+
+
+# flags as recommanded in http://grass.osgeo.org/grass70/source/INSTALL
+sudo CFLAGS="-O2 -Wall" LDFLAGS="-s" ./configure \
+  --with-cxx \
+  --with-sqlite \
+  --with-blas \
+  --enable-largefile \
+  --with-readline \
+  --without-tcltk \
+  --with-freetype \
+  --without-opengl \
+  --with-freetype-includes=/usr/include/freetype2 \
+  --with-proj-share=/usr/share/proj \
+  --without-x \
+  --without-wxwidgets
+
+sudo make
+sudo make install
+cd ..
+# compile r.walk.accessmod
+git clone https://github.com/fxi/rWalkAccessmod.git rWalkAccessmod
+cd rWalkAccessmod
+sudo make MODULE_TOPDIR=/usr/local/grass-7.0.0beta3
+
+
+
+apt-get autoclean
+apt-get autoremove
 
 
