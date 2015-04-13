e932ff5537a4ef43496b881c2919a81784778a7f
fxi <f@fxi.io>
2015-04-13 15:00:34 +0200

    removed host-only config, added packages R, changed shiny user install steps

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@
 #  /_/  |_|\___/ \___/ \___//____//____//_/  /_/ \____/ \__,_/  /_____/   
 #                                                                         
 # Author : Fred Moser <moser.frederic@gmail.com>
-# Date : 7 january 2015
+# Date : 3 april 2015
 # 
 # Script to configure virtual box VM using Vagrant.
 # Provisioning is done with provision.sh
@@ -28,21 +28,33 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # provision : shell script
   config.vm.provision "shell", path: "provision.sh"
   # Create a forwarded port mapping which allows access to a specific port
-  #config.vm.network "forwarded_port", guest: 80, host: 8080
+  # e.g. http://localhost:8080
   config.vm.network "forwarded_port", guest: 3838, host: 8080
-  # Create a private network, which allows host-only access to the machine
-  config.vm.network "private_network", ip: "192.168.38.38"
+  # Create a private network, which allows host-only access to the machine.
+  # This step configures automatically host-only adapter in virtualbox machine.
+  # In case of using directly OVA, we lost automatic conf and vagrant benefits:
+  # The user needs to add and configure its own config. TODO:check how to handle this.
+  # In case of IP conflict, uncomment dhcp line above.
+  # config.vm.network "private_network", ip: "192.168.38.38"
+  # config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
+  # configure hostname
   config.vm.hostname = "accessmod"
   # set sync folder config.vm.sync_folder "host", "guest"
   # config.vm.synced_folder "/Users/fxi/Public/share/maps", "/sharedFile"
+  # Disable default folder sync : not needed.
+  config.vm.synced_folder ".", "/vagrant", disabled: true
   
-  # http://serverfault.com/questions/495914/vagrant-slow-internet-connection-in-guest
+  # Additional setup 
   config.vm.provider "virtualbox" do |v|
+    # For 3 cards, set type of network hardware to virtualize or software interface to use
+    # virtio = not virtualized by virtualbox, but interface expected from guest : 
     v.customize ["modifyvm", :id, "--nictype1", "virtio"]
     v.customize ["modifyvm", :id, "--nictype2", "virtio"]
     v.customize ["modifyvm", :id, "--nictype3", "virtio"]
+    # Using the host's resolver as a DNS proxy in NAT mode
     v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+    # select chipset (I/O controler hub) emulation to use. ich9 may be faster
     v.customize ["modifyvm", :id, "--chipset", "ich9"]
     end
 
--- provision.sh
+++ provision.sh
@@ -16,143 +16,166 @@
 # TODO : avoid individual apt-get command
 
 
+#if [ 0 -eq 1 ] 
+#then 
 
-# in case of multiple provisioning, skip some step.
-# if any of the additional ppa is already present, skipping.
-if [ `cat /etc/apt/sources.list | grep 'ubuntugis  \| grass-stable \| cran' | wc -l` -eq 0 ]
-then
-  echo "adding ppa and key in /etc/apt/sources-list"
-  # ubuntugis ppa to source
-  sudo echo "deb http://ppa.launchpad.net/ubuntugis/ppa/ubuntu precise main" >> /etc/apt/sources.list && \
-    sudo echo "deb-src http://ppa.launchpad.net/ubuntugis/ppa/ubuntu precise main" >> /etc/apt/sources.list && \
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160 && \
-    # grass ppa to source
-  sudo echo "deb http://ppa.launchpad.net/grass/grass-stable/ubuntu trusty main" >> /etc/apt/sources.list && \ 
-  sudo echo "deb-src http://ppa.launchpad.net/grass/grass-stable/ubuntu trusty main" >> /etc/apt/sources.list && \
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 26D57B27 && \
-    # R ppa to source
-  sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list && \
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-else
-  echo "ppa seems already installed, skipping for all"
-fi
-
-# grass, R and geo tools
-# apt-get didn't like multiline arguments : sending one by one. NOTE: Why?
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y build-essential 
-sudo apt-get install -y curl 
-sudo apt-get install -y gdebi-core 
-sudo apt-get install -y pandoc 
-sudo apt-get install -y pandoc-citeproc 
-sudo apt-get install -y libcurl4-gnutls-dev 
-sudo apt-get install -y libxt-dev 
-sudo apt-get install -y r-base 
-sudo apt-get install -y r-base-dev 
-sudo apt-get install -y lesstif2-dev 
-sudo apt-get install -y dpatch 
-#sudo apt-get install -y libtiff4-dev 
-sudo apt-get install -y libfftw3-dev 
-sudo apt-get install -y libxmu-dev 
-#sudo apt-get install -y libfreetype6-dev 
-sudo apt-get install -y autoconf2.13 
-sudo apt-get install -y autotools-dev 
-sudo apt-get install -y doxygen 
-sudo apt-get install -y libsqlite3
-sudo apt-get install -y flex 
-sudo apt-get install -y bison 
-sudo apt-get install -y libgeos-dev 
-sudo apt-get install -y libgeos-3.4.2 -dev 
-#sudo apt-get install -y libcairo2-dev 
-sudo apt-get install -y gdal-bin 
-sudo apt-get install -y libgdal1-dev  
-sudo apt-get install -y libgdal1-1.5.0 
-sudo apt-get install -y libproj-dev  
-sudo apt-get install -y libproj0 
-sudo apt-get install -y proj-data 
-sudo apt-get install -y libgsl0-dev
-#sudo apt-get install -y python-numpy
-#sudo apt-get install -y python-dev
-#sudo apt-get install -y wx-common
-#sudo apt-get install -y python-wxgtk2.8
-sudo apt-get install -y git
-sudo apt-get install -y libv8-dev #V8 JavaScript engine used by geojsonio package in R
-
-# script to install shiny server.
-mkdir -p $HOME/downloads
-cd $HOME/downloads
-
-# Download and install libssl 0.9.8 
-if [ `sudo dpkg-query  -l | grep libssl0.9.8 | wc -l` -eq 0 ]
-then
-  sudo wget --no-verbose http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
-    sudo dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
-    sudo  rm -f libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
-else
-  echo "libssl0.9.8 already installed" 
-fi
-
-
-# Download and install shiny server
-if [ `sudo dpkg-query  -l | grep shiny-server | wc -l` -eq 0 ]
-then
-  echo "Download and install shiny-server"
-
-  # get the version to wget
-  sudo wget --no-verbose https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/VERSION -O "version.txt" && \
-    VERSION=$(cat version.txt)  && \
-    sudo wget --no-verbose "https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/shiny-server-$VERSION-amd64.deb" -O ss-latest.deb && \
-    sudo  gdebi -n ss-latest.deb && \
-    sudo  rm -f version.txt ss-latest.deb 
-  sudo echo 'options("repos"="http://cran.rstudio.com")' >> /etc/R/Rprofile.site
-
-  #sudo cp -R /usr/local/lib/R/site-library/shiny/examples/* /srv/shiny-server/
-else
-  echo "Shiny-server already installed"
-fi
-
-# remove default shiny sample apps :
-sudo rm -rf /srv/shiny-server/*
+  echo " set source repository "
+  echo "in case of multiple provisioning, skip some step."
+  echo "if any of the additional ppa is already present, skipping."
+  if [ `cat /etc/apt/sources.list | grep 'ubuntugis  \| grass-stable \| cran' | wc -l` -eq 0 ]
+  then
+    echo "adding ppa and key in /etc/apt/sources-list"
+    echo" ubuntugis ppa to source"
+    sudo echo "deb http://ppa.launchpad.net/ubuntugis/ppa/ubuntu precise main" >> /etc/apt/sources.list && \
+      sudo echo "deb-src http://ppa.launchpad.net/ubuntugis/ppa/ubuntu precise main" >> /etc/apt/sources.list && \
+      sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160 && \
+      echo "grass ppa to source"
+    sudo echo "deb http://ppa.launchpad.net/grass/grass-stable/ubuntu trusty main" >> /etc/apt/sources.list && \ 
+    sudo echo "deb-src http://ppa.launchpad.net/grass/grass-stable/ubuntu trusty main" >> /etc/apt/sources.list && \
+      sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 26D57B27 && \
+      echo " R ppa to source"
+    sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list && \
+      sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+  else
+    echo "ppa seems already installed, skipping for all"
+  fi
 
 
 
-# R packages and dependencies of accessmod shiny to downloads
-sudo R -e "install.packages(c(\
-  'shiny',\
-  'rmarkdown',\
-  'gdalUtils',\
-  'rgrass7',\
-  'raster',\
-  'rgdal',\
-  'rgeos',\
-  'rjson',\
-  'RSQLite',\
-  'gdata',\
-  'maps',\
-  'RSQLite',\
-  'htmltools',\
-  'devtools',\
-  'plyr'\
-  ))
-"
-sudo  R -e "devtools::install_github('fxi/AccessMod_leaflet-shiny')"
-sudo  R -e "devtools::install_github('rstudio/shinydashboard')"
-sudo  R -e "devtools::install_github('ropensci/geojsonio')"
 
 
-## install accessmod shiny
-sudo su shiny
-mkdir -p /srv/shiny-server/data/grass
-mkdir -p /srv/shiny-server/logs
-git clone https://github.com/fxi/AccessMod_shiny.git /srv/shiny-server/accessmod
-# save provisioning time in logsi
-echo -e `date +"%Y-%m-%d"`" \t log \t vagrant provisioning date" >> /srv/shiny-server/logs/logs.txt
-# index.html : redirection. Could also be a welcome screen or something.
-# if no usage of this page is done, change config file (/etc/shiny-server/shiny-server.conf )
-echo "<html><head><meta http-equiv=\"refresh\" content=\"0; url=accessmod\"></head></html>" > /srv/shiny-server/index.html
-exit
 
+  # apt-get didn't like multiline arguments : sending one by one. NOTE: Why?
+  echo "install packages"
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get install -y build-essential 
+  sudo apt-get install -y curl 
+  sudo apt-get install -y gdebi-core 
+  sudo apt-get install -y pandoc 
+  sudo apt-get install -y pandoc-citeproc 
+  sudo apt-get install -y libcurl4-gnutls-dev 
+  sudo apt-get install -y libxt-dev 
+  sudo apt-get install -y r-base 
+  sudo apt-get install -y r-base-dev 
+  sudo apt-get install -y lesstif2-dev 
+  sudo apt-get install -y dpatch 
+  #sudo apt-get install -y libtiff4-dev 
+  sudo apt-get install -y libfftw3-dev 
+  sudo apt-get install -y libxmu-dev 
+  #sudo apt-get install -y libfreetype6-dev 
+  sudo apt-get install -y autoconf2.13 
+  sudo apt-get install -y autotools-dev 
+  sudo apt-get install -y doxygen 
+  sudo apt-get install -y libsqlite3
+  sudo apt-get install -y flex 
+  sudo apt-get install -y bison 
+  sudo apt-get install -y libgeos-dev 
+  sudo apt-get install -y libgeos-3.4.2 -dev 
+  #sudo apt-get install -y libcairo2-dev 
+  sudo apt-get install -y gdal-bin 
+  sudo apt-get install -y libgdal1-dev  
+  sudo apt-get install -y libgdal1-1.5.0 
+  sudo apt-get install -y libproj-dev  
+  sudo apt-get install -y libproj0 
+  sudo apt-get install -y proj-data 
+  sudo apt-get install -y libgsl0-dev
+  #sudo apt-get install -y python-numpy
+  #sudo apt-get install -y python-dev
+  #sudo apt-get install -y wx-common
+  #sudo apt-get install -y python-wxgtk2.8
+  sudo apt-get install -y git
+  sudo apt-get install -y libv8-dev #V8 JavaScript engine used by geojsonio package in R
+  sudo apt-get install -y sqlite
+
+
+
+
+
+  echo "Install shiny server if needed"
+  mkdir -p $HOME/downloads
+  cd $HOME/downloads
+
+  # Download and install libssl 0.9.8 
+  if [ `sudo dpkg-query  -l | grep libssl0.9.8 | wc -l` -eq 0 ]
+  then
+    sudo wget --no-verbose http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
+      sudo dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb && \
+      sudo  rm -f libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
+  else
+    echo "libssl0.9.8 already installed" 
+  fi
+
+
+  # Download and install shiny server
+  if [ `sudo dpkg-query  -l | grep shiny-server | wc -l` -eq 0 ]
+  then
+    echo "Download and install shiny-server"
+
+    # get the version to wget
+    sudo wget --no-verbose https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/VERSION -O "version.txt" && \
+      VERSION=$(cat version.txt)  && \
+      sudo wget --no-verbose "https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/shiny-server-$VERSION-amd64.deb" -O ss-latest.deb && \
+      sudo  gdebi -n ss-latest.deb && \
+      sudo  rm -f version.txt ss-latest.deb 
+    sudo echo 'options("repos"="http://cran.rstudio.com")' >> /etc/R/Rprofile.site
+
+    #sudo cp -R /usr/local/lib/R/site-library/shiny/examples/* /srv/shiny-server/
+  else
+    echo "Shiny-server already installed"
+  fi
+
+  # remove default shiny sample apps :
+  sudo rm -rf /srv/shiny-server/*
+
+
+  echo "Install R packages"
+  sudo mkdir -p /srv/shiny-server/data/grass
+  sudo mkdir -p /srv/shiny-server/data/cache
+  sudo mkdir -p /srv/shiny-server/logs
+  sudo mkdir -p /srv/shiny-server/libs
+  sudo mkdir -p /srv/shiny-server/help
+
+  # R package and dependecies
+
+  sudo Rscript -e "install.packages(c('devtools','shiny','RSQLite'))"
+  sudo Rscript -e "devtools::install_github('fxi/AccessMod_leaflet-shiny')"
+  sudo Rscript -e "devtools::install_github('rstudio/shinydashboard')"
+
+
+  # R packages and dependencies of accessmod shiny to downloads
+  sudo Rscript -e "install.packages(lib='/srv/shiny-server/libs',c('htmltools','rmarkdown','gdalUtils','rgrass7','raster','rgdal','rgeos','RSQLite','gdata','maps','plyr','pingr','V8','data.table','maptools','dplyr','gistr','magrittr','haven', 'openxlsx', 'readODS'))"
+
+
+#fi;
+
+
+echo "Install AccessMod web app"
+sudo rm -rf /srv/shiny-server/accessmod 2> /dev/null
+sudo git clone https://github.com/fxi/AccessMod_shiny.git /srv/shiny-server/accessmod
+
+sudo unzip /srv/shiny-server/accessmod/demo/demo.zip
+sudo mv demo /srv/shiny-server/data/grass/demo
+
+sudo echo "<html><head><meta http-equiv=\"refresh\" content=\"0; url=accessmod\"></head></html>" > $HOME/index.html
+sudo echo echo -e `date +"%Y-%m-%d"`" \t log \t vagrant provisioning date" > $HOME/logs.txt
+sudo mv $HOME/index.html /srv/shiny-server/index.html
+sudo mv $HOME/logs.txt /srv/shiny-server/logs/logs.txt
+
+sudo chown -R shiny:shiny /srv/shiny-server/
+
+
+#
+#sudo su shiny << EOF 
+#echo "Cloning AccessMod to /srv/shiny-server/accessmod"
+#git clone https://github.com/fxi/AccessMod_shiny.git /srv/shiny-server/accessmod
+## save provisioning time in logs
+#echo -e `date +"%Y-%m-%d"`" \t log \t vagrant provisioning date" >> /srv/shiny-server/logs/logs.txt
+## index.html : redirection. Could also be a welcome screen or something.
+## if no usage of this page is done, change config file (/etc/shiny-server/shiny-server.conf )
+#EOF
+
+echo "Compile and install grass"
 
 # install grass and r.walk.accessmod
 mkdir -p $HOME/downloads
@@ -185,51 +208,51 @@ CFLAGS="-O2 -Wall -march=x86-64 -mtune=native" LDFLAGS="-s" ./configure \
   --without-x \
   --without-wxwidgets
 
-# configuration summary
-#GRASS is now configured for:  x86_64-unknown-linux-gnu
-#
-#Source directory:           /home/vagrant/downloads/grass-7.0.0
-#Build directory:            /home/vagrant/downloads/grass-7.0.0
-#Installation directory:     ${prefix}/grass-7.0.0
-#Startup script in directory:${exec_prefix}/bin
-#C compiler:                 gcc -O2 -Wall -march=x86-64 -mtune=native
-#C++ compiler:               c++ -g -O2
-#Building shared libraries:  yes
-#OpenGL platform:            none
-#
-#MacOSX application:         no
-#MacOSX architectures:
-#MacOSX SDK:
-#
-#BLAS support:               no
-#C++ support:                yes
-#Cairo support:              no
-#DWG support:                no
-#FFTW support:               yes
-#FreeType support:           no
-#GDAL support:               yes
-#GEOS support:               yes
-#LAPACK support:             no
-#Large File support (LFS):   yes
-#libLAS support:             no
-#MySQL support:              no
-#NetCDF support:             no
-#NLS support:                no
-#ODBC support:               no
-#OGR support:                yes
-#OpenCL support:             no
-#OpenGL support:             no
-#OpenMP support:             no
-#PNG support:                yes
-#POSIX thread support:       no
-#PostgreSQL support:         no
-#Readline support:           yes
-#Regex support:              yes
-#SQLite support:             yes
-#TIFF support:               no
-#wxWidgets support:          no
-#X11 support:                no
-#
+#- configuration summary
+#-GRASS is now configured for:  x86_64-unknown-linux-gnu
+#-
+#-Source directory:           /home/vagrant/downloads/grass-7.0.0
+#-Build directory:            /home/vagrant/downloads/grass-7.0.0
+#-Installation directory:     ${prefix}/grass-7.0.0
+#-Startup script in directory:${exec_prefix}/bin
+#-C compiler:                 gcc -O2 -Wall -march=x86-64 -mtune=native
+#-C++ compiler:               c++ -g -O2
+#-Building shared libraries:  yes
+#-OpenGL platform:            none
+#-
+#-MacOSX application:         no
+#-MacOSX architectures:
+#-MacOSX SDK:
+#-
+#-BLAS support:               no
+#-C++ support:                yes
+#-Cairo support:              no
+#-DWG support:                no
+#-FFTW support:               yes
+#-FreeType support:           no
+#-GDAL support:               yes
+#-GEOS support:               yes
+#-LAPACK support:             no
+#-Large File support (LFS):   yes
+#-libLAS support:             no
+#-MySQL support:              no
+#-NetCDF support:             no
+#-NLS support:                no
+#-ODBC support:               no
+#-OGR support:                yes
+#-OpenCL support:             no
+#-OpenGL support:             no
+#-OpenMP support:             no
+#-PNG support:                yes
+#-POSIX thread support:       no
+#-PostgreSQL support:         no
+#-Readline support:           yes
+#-Regex support:              yes
+#-SQLite support:             yes
+#-TIFF support:               no
+#-wxWidgets support:          no
+#-X11 support:                no
+#-
 
 
 sudo make # no root access needed here... but permission error (in demolocation??) when installing custom module. Strange.
@@ -239,6 +262,46 @@ cd $HOME/downloads
 git clone https://github.com/fxi/AccessMod_r.walk.git AccessMod_r_walk
 cd AccessMod_r_walk
 sudo make MODULE_TOPDIR=/usr/local/grass-7.0.0
+
+
+
+
+#amip=`ip addr | grep -Po "(?!(inet 127.\d.\d.1))(?!(inet 10.\d.\d.\d))(inet \K(\d{1,3}\.){3}\d{1,3})"`
+
+# set message if Virtual machine is launched directly 
+echo '#!/bin/sh
+if [ "$METHOD" = loopback ]; then
+  exit 0
+fi
+
+# Only run from ifup.
+if [ "$MODE" != start ]; then
+  exit 0
+fi
+
+# cp /etc/issue-orig /etc/issue
+echo "                                                " > /etc/issue
+echo " _____                     _____       _    ___ " >> /etc/issue
+echo "|  _  |___ ___ ___ ___ ___|     |___ _| |  |  _|" >> /etc/issue
+echo "|     |  _|  _| -_|_ -|_ -| | | | . | . |  |_  |" >> /etc/issue
+echo "|__|__|___|___|___|___|___|_|_|_|___|___|  |___|" >> /etc/issue
+echo "                                                " >> /etc/issue
+echo "#-----------------------------------------------#" >> /etc/issue
+echo " To launch AcessMod:" >> /etc/issue
+echo " Type this URL in a browser or launch AccessMod_client" >> /etc/issue
+echo " http://localhost:8080">> /etc/issue
+echo "#-----------------------------------------------#" >> /etc/issue
+echo " Please report any issue :" >> /etc/issue
+echo "   - Web application:  https://github.com/fxi/AccessMod_shiny" >> /etc/issue
+echo "   - Virtual machine:  https://github.com/fxi/AccessMod_server" >> /etc/issue
+echo "   - Cumulative cost function: https://github.com/fxi/AccessMod_r.walk" >> /etc/issue
+echo "#-----------------------------------------------#" >> /etc/issue
+echo "Email: Fred Moser f@fxi.io"
+' > accessmodmessage
+
+sudo chmod +x accessmodmessage
+sudo mv accessmodmessage /etc/network/if-up.d/accessmodmessage
+
 
 # clean 
 sudo apt-get autoclean
